0e4c106e9c23c6bdbe025c8b848aaad38f3ad495
Kielvien L E S Putra <for2university@gmail.com>
2025-09-23 08:53:06 +0700

    init project purchase order

---
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/PurchaseorderApplication.java
@@ -0,0 +1,13 @@
+package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/PurchaseorderApplicationTests.java
@@ -0,0 +1,13 @@
+package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
