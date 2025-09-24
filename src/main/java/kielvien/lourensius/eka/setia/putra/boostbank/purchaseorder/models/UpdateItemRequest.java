e769d4bfd755142fbfc3f80ce5bf7cf213c7bef5
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 05:29:32 +0700

    feature/developmennt: Adding field for item update item request and response

--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/UpdateItemRequest.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/UpdateItemRequest.java
@@ -1,5 +1,23 @@
 package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models;
 
-public class UpdateItemRequest {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
+public class UpdateItemRequest {
+	@NotBlank(message = "cannot be null or empty")
+	@Size(max = 500, message = "character cannot more then 500")
+	private String name;
+
+	@NotBlank(message = "cannot be null or empty")
+	@Size(max = 500, message = "character cannot more then 500")
+	private String description;
+
+	@Min(value = 1, message = "cannot less then 1")
+	private int price;
+
+	@Min(value = 1, message = "cannot less then 1")
+	private int cost;
 }
--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/UpdateItemResponse.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/UpdateItemResponse.java
@@ -1,5 +1,13 @@
 package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models;
 
-public class UpdateItemResponse {
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
+public class UpdateItemResponse {
+	private String name;
+	private String description;
+	private int price;
+	private int cost;
 }
