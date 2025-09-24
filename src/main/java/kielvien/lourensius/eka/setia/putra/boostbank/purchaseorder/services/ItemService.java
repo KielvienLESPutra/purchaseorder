a7cb8dd288b1ff46000c00d80d591da081ada107
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 05:20:07 +0700

    feature/development: Adding method to find item and method to return item from db

--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/services/ItemService.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/services/ItemService.java
@@ -1,11 +1,15 @@
 package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.services;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.constants.Constants;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.entities.Items;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.CreateItemRequest;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.CreateItemResponse;
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.GetItemResponse;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.repository.ItemRepository;
 
 @Service
@@ -31,5 +35,17 @@ public class ItemService {
 		itemRepository.save(item);
 		return CreateItemResponse.builder().name(item.getName()).description(item.getDescription())
 				.price(item.getPrice()).cost(item.getCost()).build();
+	}
+
+	public Items getUserById(int itemId) {
+		return itemRepository.findById(itemId).orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND, Constants.statusCode.NOT_FOUND.getDesc()));
+	}
+
+	public GetItemResponse findItem(int itemId) {
+		Items item = getUserById(itemId);
+		
+		return GetItemResponse.builder().name(item.getName()).description(item.getDescription()).price(item.getPrice())
+				.cost(item.getCost()).build();
 	}
 }
