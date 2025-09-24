fb3ea3dda926eb29bdcb23e13f344bd34151b38c
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 04:42:29 +0700

    feature/development: Adding field for create item request and validation

--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/CreateItemRequest.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/CreateItemRequest.java
@@ -1,5 +1,28 @@
 package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models;
 
-public class CreateItemRequest {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
+public class CreateItemRequest {
+	@NotBlank(message = "cannot be null or empty")
+	@Size(max = 500, message = "character cannot more then 500")
+	private String name;
+
+	@NotBlank(message = "cannot be null or empty")
+	@Size(max = 500, message = "character cannot more then 500")
+	private String description;
+
+	@Min(value = 1, message = "cannot less then 1")
+	@Pattern(regexp = "^\\d*$", message = "pattern is invalid")
+	private int price;
+
+	@Min(value = 1, message = "cannot less then 1")
+	@Pattern(regexp = "^\\d*$", message = "pattern is invalid")
+	private int cost;
 }
