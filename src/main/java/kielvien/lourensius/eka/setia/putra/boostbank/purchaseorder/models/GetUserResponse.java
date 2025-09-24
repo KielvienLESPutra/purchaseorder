109e091e811e06fed024faa181e8913d773f76dd
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 00:40:55 +0700

    feature/development: Adding get user response class

---
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/GetUserResponse.java
@@ -0,0 +1,13 @@
+package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class GetUserResponse {
+	private String firstName;
+	private String lastName;
+	private String phone;
+	private String email;
+}
