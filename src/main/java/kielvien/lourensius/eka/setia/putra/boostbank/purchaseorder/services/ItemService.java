01197916ca02e4296d794562d3fa6718ad2da95f
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 05:10:27 +0700

    feature/development: Addinng service to create data item

--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/services/ItemService.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/services/ItemService.java
@@ -1,14 +1,35 @@
 package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.entities.Items;
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.CreateItemRequest;
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.CreateItemResponse;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.repository.ItemRepository;
 
 @Service
 public class ItemService {
 	private ItemRepository itemRepository;
+	private ValidationService validationService;
 
-	public ItemService(ItemRepository itemRepository) {
+	public ItemService(ItemRepository itemRepository, ValidationService validationService) {
 		this.itemRepository = itemRepository;
+		this.validationService = validationService;
+	}
+
+	@Transactional
+	public CreateItemResponse createItem(CreateItemRequest request) {
+		validationService.validate(request);
+		Items item = new Items();
+
+		item.setName(request.getName());
+		item.setDescription(request.getDescription());
+		item.setPrice(request.getPrice());
+		item.setCost(request.getCost());
+
+		itemRepository.save(item);
+		return CreateItemResponse.builder().name(item.getName()).description(item.getDescription())
+				.price(item.getPrice()).cost(item.getCost()).build();
 	}
 }
