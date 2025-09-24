c4c618a6adafd63040b44a8374246985b10f041a
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 05:03:12 +0700

    feature/development: Refactor code remove builder and fixing validation remove pattern

--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/CreateItemRequest.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/CreateItemRequest.java
@@ -2,13 +2,10 @@ package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class CreateItemRequest {
 	@NotBlank(message = "cannot be null or empty")
 	@Size(max = 500, message = "character cannot more then 500")
@@ -19,10 +16,8 @@ public class CreateItemRequest {
 	private String description;
 
 	@Min(value = 1, message = "cannot less then 1")
-	@Pattern(regexp = "^\\d*$", message = "pattern is invalid")
 	private int price;
 
 	@Min(value = 1, message = "cannot less then 1")
-	@Pattern(regexp = "^\\d*$", message = "pattern is invalid")
 	private int cost;
 }
