aef9902db245402eb7c481b081842636fb7dc66c
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 04:08:40 +0700

    feature/development: Init item repository interface class

---
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/repository/ItemRepository.java
@@ -0,0 +1,11 @@
+package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.entities.Items;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Items, Integer>{
+
+}
