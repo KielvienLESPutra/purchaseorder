c61012b6ffbf0ba607bca60057133112222af76d
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 02:15:12 +0700

    feature/development: Adding method to update user data

--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/services/UserService.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/services/UserService.java
@@ -10,6 +10,8 @@ import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.entities.User
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.CreateUserRequest;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.CreateUserResponse;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.GetUserResponse;
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.UpdateUserRequest;
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.UpdateUserResponse;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.repository.UserRepository;
 
 @Service
@@ -39,10 +41,27 @@ public class UserService {
 	}
 
 	public GetUserResponse getUser(int id) {
-		User user = userRepository.findById(id)
-				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, Constants.statusCode.NOT_FOUND.getDesc()));
+		User user = userRepository.findById(id).orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND, Constants.statusCode.NOT_FOUND.getDesc()));
 
 		return GetUserResponse.builder().firstName(user.getFirstName()).lastName(user.getLastName())
+				.phone(user.getPhone()).email(user.getEmail()).build();
+	}
+
+	@Transactional
+	public UpdateUserResponse updateUser(int userid, UpdateUserRequest request) {
+		validationService.validate(request);
+		
+		User user = userRepository.findById(userid).orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND, Constants.statusCode.NOT_FOUND.getDesc()));
+
+		user.setFirstName(request.getFirstName());
+		user.setLastName(request.getLastName());
+		user.setPhone(request.getPhone());
+		user.setEmail(request.getEmail());
+		userRepository.save(user);
+
+		return UpdateUserResponse.builder().firstName(user.getFirstName()).lastName(user.getLastName())
 				.phone(user.getPhone()).email(user.getEmail()).build();
 	}
 }
