27989231ecf007dc9b7af9a7b4106163f766d355
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 11:29:51 +0700

    feature/developmennt: Adding create data for purchase order

--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/controller/PurchaseOrderController.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/controller/PurchaseOrderController.java
@@ -1,8 +1,15 @@
 package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.constants.Constants;
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.CreatePurchaseOrderRequest;
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.CreatePurchaseOrderResponse;
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.WebResponse;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.services.PurchaseOrderService;
 
 @RestController
@@ -12,5 +19,14 @@ public class PurchaseOrderController {
 
 	public PurchaseOrderController(PurchaseOrderService orderService) {
 		this.orderService = orderService;
+	}
+
+	@PostMapping(path = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public WebResponse<CreatePurchaseOrderResponse> createPurchaseOrder(
+			@RequestBody CreatePurchaseOrderRequest request) {
+		CreatePurchaseOrderResponse response = orderService.createPurchaseOrder(request);
+
+		return WebResponse.<CreatePurchaseOrderResponse>builder().data(response)
+				.statusCode(Constants.statusCode.OK.getCode()).desc(Constants.statusCode.OK.getDesc()).build();
 	}
 }
