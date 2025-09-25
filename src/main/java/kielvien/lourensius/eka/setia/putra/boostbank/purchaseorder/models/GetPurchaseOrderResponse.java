39b10f781a530d2b5ea27b97a175c4d81e7447fa
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 11:51:57 +0700

    feature/developmennt: Adding field for get purchase order response

--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/GetPurchaseOrderResponse.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/GetPurchaseOrderResponse.java
@@ -1,10 +1,15 @@
 package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models;
 
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class GetPurchaseOrderResponse {
-
+	private String description;
+	private int totalPrice;
+	private int totalCost;
+	private List<PurchaseOderDetailModel> purchaseOrderDetails;
 }
