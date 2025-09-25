c18645453d28cda1be615206922343726e56cfd6
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 11:25:58 +0700

    feature/developmennt: Adding enum status code reference not found

--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/constants/Constants.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/constants/Constants.java
@@ -2,9 +2,10 @@ package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.constants;
 
 public class Constants {
 	public static final String USER_SYSTEM = "SYSTEM";
-	
+
 	public enum statusCode {
-		OK("00", "SUCCESS"), BAD_REQUEST("01", "BAD REQUEST"), NOT_FOUND("02","NOT FOUND");
+		OK("00", "SUCCESS"), BAD_REQUEST("01", "BAD REQUEST"), NOT_FOUND("02", "NOT FOUND"),
+		REFERENCE_NOT_FOUND("03", "REFERENCE NOT FOUND");
 
 		private String code;
 		private String desc;
