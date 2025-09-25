9a5c16fd81751397c5b48c7139d4507a74c209f4
Kielvien L E S Putra <for2university@gmail.com>
2025-09-25 08:06:20 +0700

    feature/developmennt: Adding validation for purchase order request and detail model

--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/PurchaseOderDetailModel.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/PurchaseOderDetailModel.java
@@ -1,5 +1,6 @@
 package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models;
 
+import jakarta.validation.constraints.Min;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,5 +8,10 @@ import lombok.Data;
 @Builder
 public class PurchaseOderDetailModel {
 	private int itemId;
+	
+	@Min(value = 1, message = "cannot less then 1")
 	private int itemQty;
+	
+	private int itemCost;
+	private int itemPrice;
 }
--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/PurchaseOrderRequest.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/models/PurchaseOrderRequest.java
@@ -2,11 +2,18 @@ package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.entities.PurchaseOrderDetail;
 import lombok.Data;
 
 @Data
 public class PurchaseOrderRequest {
+	@NotBlank(message = "cannot be null or empty")
+	@Size(max = 500, message = "character cannot more then 500")
 	private String description;
+
+	@NotBlank(message = "cannot be null or empty")
+	@Size(min = 1, message = "purchase order detail must be at least 1")
 	private List<PurchaseOrderDetail> purchaseOrderDetails;
 }
