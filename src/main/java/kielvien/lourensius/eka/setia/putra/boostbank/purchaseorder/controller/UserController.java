8363f023312c3ab4cf6802dcf999952351f07067
Kielvien L E S Putra <for2university@gmail.com>
2025-09-27 07:54:15 +0700

    feature/development: Adding endpoint find all user

--- src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/controller/UserController.java
+++ src/main/java/kielvien/lourensius/eka/setia/putra/boostbank/purchaseorder/controller/UserController.java
@@ -1,5 +1,8 @@
 package kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.controller;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.constants.Constants;
@@ -16,6 +20,7 @@ import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.Create
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.GetUserResponse;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.UpdateUserRequest;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.UpdateUserResponse;
+import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.WebPageResponse;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.models.WebResponse;
 import kielvien.lourensius.eka.setia.putra.boostbank.purchaseorder.services.UserService;
 
@@ -43,6 +48,21 @@ public class UserController {
 
 		return WebResponse.<GetUserResponse>builder().data(response).statusCode(Constants.statusCode.OK.getCode())
 				.desc(Constants.statusCode.OK.getDesc()).build();
+	}
+
+	@GetMapping(path = "/finduser", produces = MediaType.APPLICATION_JSON_VALUE)
+	public WebResponse<List<GetUserResponse>> getAllUser(
+			@RequestParam(defaultValue = "0", required = false) Integer page,
+			@RequestParam(defaultValue = "10", required = false) Integer totalSize) {
+		Page<GetUserResponse> response = userService.findAllUser(page, totalSize);
+		WebPageResponse pages = new WebPageResponse();
+		pages.setCurrentPage(response.getNumber());
+		pages.setTotalPage(response.getTotalPages());
+		pages.setSize(response.getSize());
+
+		return WebResponse.<List<GetUserResponse>>builder().data(response.getContent())
+				.statusCode(Constants.statusCode.OK.getCode()).desc(Constants.statusCode.OK.getDesc()).paging(pages)
+				.build();
 	}
 
 	@PutMapping(path = "/update/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
